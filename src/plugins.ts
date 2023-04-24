d31689458a5b21d6ce8c59e707880f145bbba4bd
IIIMADDINIII <martin@elend.name>
2023-04-24 21:39:06 +0200

    fixing sourcemap warning

--- src/plugins.ts
+++ src/plugins.ts
@@ -2,21 +2,29 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from "@rollup/plugin-json";
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from "@rollup/plugin-terser";
-import typescript from "@rollup/plugin-typescript";
+import typescript, { RollupTypescriptOptions } from "@rollup/plugin-typescript";
 import type { Plugin } from "rollup";
 import { consts } from 'rollup-plugin-consts';
 import sourceMaps from 'rollup-plugin-include-sourcemaps';
 import { manageDepsPlugin } from "./manageDepsPlugin.js";
 import type { Config } from "./readConfig.js";
 
-export function getPlugins(config: Config, discardDeclarations: boolean = false): Plugin[] {
+function getTypescriptOptions(config: Config, discardDeclarations: boolean = false): RollupTypescriptOptions {
   const generateDeclaration = config.generateDeclaration && !discardDeclarations;
+  let ret: RollupTypescriptOptions = { noEmitOnError: true, outputToFilesystem: true, declaration: generateDeclaration, declarationMap: generateDeclaration };
+  if (config.production) {
+    ret.sourceMap = false;
+  }
+  return ret;
+}
+
+export function getPlugins(config: Config, discardDeclarations: boolean = false): Plugin[] {
   let plugins = [
     manageDepsPlugin(config),
     consts({ production: config.production, development: !config.production }),
     json(),
     commonjs(),
-    typescript({ noEmitOnError: true, outputToFilesystem: true, declaration: generateDeclaration, declarationMap: generateDeclaration }),
+    typescript(getTypescriptOptions(config, discardDeclarations)),
     sourceMaps(),
     nodeResolve(),
   ];
