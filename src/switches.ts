3afc520f6ec4b4ae4ce6931b3bf14d8998256f78
IIIMADDINIII <martin@elend.name>
2023-04-24 21:33:13 +0200

    disdributing switches declarations

--- src/switches.ts
+++ src/switches.ts
@@ -2,6 +2,7 @@ declare module "consts:production" {
   let production: boolean;
   export default production;
 }
+
 declare module "consts:development" {
   let development: boolean;
   export default development;
